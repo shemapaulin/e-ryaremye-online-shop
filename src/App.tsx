0fd40d86bc22a2bcde440cb7b38b03bd824a2e3b
Shema <shema@post.com>
2026-07-17 01:42:35 +0200

    chakra ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import { Button,HStack } from "@chakra-ui/react"
 
 function App() {
 
   return (
     <>
-      
+      <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
+import { Provider } from './components/ui/provider.tsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider >
     <App />
+    </Provider>
   </StrictMode>,
 )
