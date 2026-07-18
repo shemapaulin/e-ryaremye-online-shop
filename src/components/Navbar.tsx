a0fd972b8ea7078f14e1ca40fd3a35195b0c92ed
Shema <shema@post.com>
2026-07-18 05:39:34 +0200

    color Mode Switch Activated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react"
 import Isoko from "../assets/isoko.png"
+import ColorModeSwitch from "./ColorModeSwitch"
 
 
 const Navbar = () => {
   return (
-    <HStack >
+    <HStack justifyContent={'space-between'} >
         <Image src={Isoko} boxSize='90px' boxShadow='5px'/>
+        <ColorModeSwitch/>
     </HStack>
   )
 }
